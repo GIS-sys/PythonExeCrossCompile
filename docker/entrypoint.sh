d3b66269d95b1c570c29413f0a53e042eb8651bc
GISwarm <gordeiegorov@gmail.com>
2026-02-06 10:00:14 +0300

    attempt to install conda

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -27,10 +27,42 @@ if [[ $installed_version != $required_version ]]; then
     winetricks -q win10
 
     wine python --version
+    echo -e "\n\nA\n\n"
     wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}-amd64.exe"
-    wine ./python-${PYTHON_VERSION}-amd64.exe
-    wine python --version
+    echo -e "\n\nB\n\n"
+    wine ./python-${PYTHON_VERSION}-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+    echo -e "\n\nC\n\n"
+    wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe" -O miniconda.exe
+    echo -e "\n\nD\n\n"
+    wine miniconda.exe tos accept --override-channels --channel https://repo.anaconda.com/pkg
+    echo -e "\n\nE\n\n"
+    wine miniconda.exe /S /InstallationType=JustMe /AddToPath=1 /RegisterPython=0
+    echo -e "\n\nF\n\n"
+
+    sleep 5
+
+    export WINEPATH="C:\\Miniconda3;C:\\Miniconda3\\Scripts;C:\\Miniconda3\\Library\\bin;$WINEPATH"
+    echo -e "\n\nG\n\n"
+    wine conda tos accept --override-channels --channel https://repo.anaconda.com/pkg
+    echo -e "\n\nH\n\n"
+    wine conda init
+    echo -e "\n\nI\n\n"
+    wine conda config --add channels conda-forge
+    echo -e "\n\nJ\n\n"
+    wine conda config --set channel_priority strict
+    echo -e "\n\nK\n\n"
+
+    wine conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+    wine conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+    wine conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/msys2
+    echo -e "\n\nKL\n\n"
+    wine conda install -c conda-forge pythonocc-core=7.7.0 -y
+    echo -e "\n\nL\n\n"
+
+    wine python -m pip install --upgrade pip
     wine pip install -r /app/docker/requirements.txt
+
+    wine python -c "import OCC; print(f'pythonocc-core успешно установлен: {OCC.__version__}')"
     if [ $? -ne 0 ]; then
         echo "Error: pip install failed"
         exit 1
@@ -41,7 +73,20 @@ if [[ $installed_version != $required_version ]]; then
     echo "Python Installation completed successfully."
 else
     echo "Python Installation already completed, skipping..."
-    wine python --version
+    wine conda config --set solver classic > /tmp/b && cat /tmp/b
+    echo -e "\n\nZ\n\n"
+    wine conda config --system --set solver classic && wine conda update --all -y
+    echo -e "\n\nY\n\n"
+    #wine conda config --set solver classic
+    #wine python --version
+    #wine conda create -n myenv python=${PYTHON_VERSION} -y
+    wine conda activate myenv
+    wine conda env list
+
+    wine conda list
+    #wine conda install -c conda-forge pythonocc-core=7.9.0 -y > /tmp/a && cat /tmp/a
+    #wine conda list
+    exit
 fi
 
 
